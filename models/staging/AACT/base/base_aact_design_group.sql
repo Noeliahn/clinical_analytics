8363bb797c061fcc6e1195b11431732c646dbee6
Noelia Heredia <noelia.h.n.1995@gmail.com>
2025-11-24 02:22:04 +0000

    20251124 - silver sin incrementales ni snapshots

--- models/staging/AACT/base/base_aact_design_group.sql
+++ models/staging/AACT/base/base_aact_design_group.sql
@@ -7,7 +7,10 @@ renamed_casted AS (
     SELECT
     id::INT                                                                     AS id_design_groups
     , UPPER(REGEXP_REPLACE(TRIM(nct_id), '[^A-Za-z0-9]', ''))::VARCHAR(256)     AS nct_id
-    , MD5(group_type)::VARCHAR(256)                                             AS group_type_id
+    , (CASE 
+        WHEN group_type IS NULL THEN MD5('no_group_type' )
+        ELSE MD5(group_type)
+    END)::VARCHAR(256)                                                          AS group_type_id
     , group_type::VARCHAR(256)                                                  AS group_type
     , title::VARCHAR(256)                                                       AS title
     , description::VARCHAR(1024)                                                AS description
@@ -17,3 +20,4 @@ renamed_casted AS (
 
 SELECT * 
 FROM renamed_casted
+
--- models/staging/AACT/base/base_aact_facilities.sql
+++ models/staging/AACT/base/base_aact_facilities.sql
@@ -7,11 +7,21 @@ renamed_casted AS (
     SELECT
     id::INT                                                                     AS id_facilities
     , UPPER(REGEXP_REPLACE(TRIM(nct_id), '[^A-Za-z0-9]', ''))::VARCHAR(256)     AS nct_id
-    , MD5(status)::VARCHAR(256)                                                 AS status_id
+
+    , (CASE 
+        WHEN status IS NULL THEN MD5('no_status' )
+        ELSE MD5(status)
+    END)::VARCHAR(256)                                                          AS status_id
     , status::VARCHAR(256)                                                      AS status
+    
     , name::VARCHAR(256)                                                        AS name
-    , MD5(country)::VARCHAR(256)                                                AS country_id
+    
+     , (CASE 
+        WHEN country IS NULL THEN MD5('no_country' )
+        ELSE MD5(country)
+    END)::VARCHAR(256)                                                          AS country_id   
     , country::VARCHAR(256)                                                     AS country
+
     , latitude                                                                  AS latitude
     , longitude                                                                 AS longitude
 
